ae7021fadc3df60fb50c53f01ef75657435784ee
Jonathan Grant <jonathanallengrant@gmail.com>
2016-02-10 10:20:24 -0800

    print card and deck methods

--- example.py
+++ example.py
@@ -1,18 +1,32 @@
-def Card:
+import random
+
+class Card:
     cardCount = 0
     
     def __init__(self, number, suit):
         self.number = number
         self.suit = suit
         Card.cardCount+=1
+    
+    def printCard(self):
+        if self.number > 10:
+            print("%i of %s."%{self.number, self.suit})
+        else:
+            print("%i of %s."%{self.number, self.suit})
         
-def Deck:
-    def __init__(self, cards, suits):
-        self.cards = cards
-        self.suits = suits
+class Deck:
+    def __init__(self, cardList, suitList):
+        self.cardList = cardList
+        self.suitList = suitList
         #Now we create the cards and put it into the deck
         self.cards = []
-        for i in cards:
-            for j in suits:
-                self.cards.append(new Card(i,j))
-        #Now shuffle deck!
+        for i in cardList:
+            for j in suitList:
+                self.cards.append(Card(i,j))
+    
+    def shuffle(self):
+        random.shuffle(self.cards)
+        
+    def printDeck(self):
+        for card in self.cards:
+            card.printCard()
